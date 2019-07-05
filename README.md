a967de27d3a2668fe981e10a8b39539f7ddc50d7
Gastón I. Silva <givanse@gmail.com>
2019-07-04 23:18:01 -0700

    first

---
+++ README.md
@@ -0,0 +1,27 @@
+# broccoli-web-components
+
+Summarize what broccoli-web-components does here.
+
+## Usage
+
+```js
+var BroccoliWebComponents = require('broccoli-web-components');
+
+module.exports = new BroccoliWebComponents(input, options);
+```
+
+## Development
+
+### Installation
+
+* `git clone <repository-url>`
+* `cd broccoli-web-components`
+* `npm install`
+
+### Building
+
+* `npm run build`
+
+### Testing
+
+* `npm run test` or `npm run test:debug`
---
+++ tests/index.js
@@ -0,0 +1,42 @@
+import 'regenerator-runtime/runtime'; // only for tests, because async/await needs it
+
+import chai from 'chai';
+import { buildOutput, createTempDir } from 'broccoli-test-helper';
+
+import BroccoliWebComponents from '../';
+
+const { expect } = chai;
+
+chai.config.truncateThreshold = 1000;
+
+describe('BroccoliWebComponents', function() {
+  let input;
+
+  beforeEach(function() {
+    return createTempDir().then(tempDir => (input = tempDir));
+  });
+
+  afterEach(function() {
+    return input.dispose();
+  });
+
+  it('should build', async function() {
+    input.write({
+      // Your fixture directory structure
+    });
+
+    let node = new BroccoliWebComponents(input.path(), {
+      // Options
+    });
+
+    let output = await buildOutput(node);
+
+    expect(output.read()).to.deep.equal({
+      // Your transformed directory structure
+    });
+
+    output = await output.rebuild();
+
+    expect(output.changes()).to.deep.equal({});
+  });
+});
